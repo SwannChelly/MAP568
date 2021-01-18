f67d01e0470f6b525fd1fd13eab8c4738bb34d4d
Maxime Vincent <max@MacBook-Air-de-Maxime.local>
2021-01-18 22:38:01 +0100

    Resolution Système

--- src/q1.py
+++ src/q1.py
@@ -4,5 +4,8 @@ from utils import *
 
 # First define the matrix : 
 
-Res = np.zeros((N_VARIABLE,T_MAX))
-Res[0,0] = S_0
+
+
+
+
+Res = Resolution_Systeme(T_MAX, deltaT,1000,S_0)
--- src/utils.py
+++ src/utils.py
@@ -21,6 +21,18 @@ def euler_one_step(S,I_moins,I_plus,R_moins,R_plus_I,H,U,R_plus_H,D,D_R):
     return(S_1,I_moins_1,I_plus_1,R_moins_1,R_plus_I_1,H_1,U_1,R_plus_H_1,D_1,D_R_1)
 
 
+def Resolution_Systeme(T_MAX, deltaT,I_moins_0,S_0):
+    Res = np.zeros((N_VARIABLE,T_MAX+1))
+    I_moins_0 = 1000
+    Res[0,0] = S_0 - I_moins_0
+    Res[1,0] = I_moins_0
+    i = 1
+    N_ETAPES = T_MAX/deltaT  
+    while i <= N_ETAPES:
+        Res[:,i] = euler_one_step(Res[0,i-1],Res[1,i-1],Res[2,i-1],Res[3,i-1],Res[4,i-1],Res[5,i-1],Res[6,i-1],Res[7,i-1],Res[8,i-1],Res[9,i-1])
+        i+=1
+    
+    return(Res)
 
 
 
