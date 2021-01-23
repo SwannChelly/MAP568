0105f502893eb2dcdaceaff44fade82e1bd97fd2
MaaaximeV <maaaxime.vincent@gmail.com>
2021-01-23 19:28:18 +0100

    odeint

--- src/q1.py
+++ src/q1.py
@@ -1,8 +1,18 @@
+from __future__ import division
 import numpy as np 
 from constant import *
 from utils import * 
 import random
 import matplotlib.pyplot as plt
+from scipy import *
+from pylab import *     
+from scipy.integrate import odeint # Module de résolution des équations différentielles
+
+
+from constant import *
+
+
+
 
 # First define the matrix : 
 
@@ -12,7 +22,52 @@ import matplotlib.pyplot as plt
 #Res = Resolution_Systeme(set_size, deltaT,S_0)
 #print(Res[1])  ##### nombre d'infectés
 
-"""
+start = 0
+end = T_MAX
+numsteps = set_size
+time = np.linspace(start,end,numsteps)
+#i = 0
+#for i in range(len(time)):
+#    time[i]=round(time[i],1)
+#time = list(time)   
+   
+
+
+
+def equa_diff(syst,temps):
+    #temps = round(temps,1)
+    #if temps == 50:
+    #    temps = 50.1
+    #elif temps > 100.:
+    #    temps = 100.
+    #i = time.index(temps)
+    tau = cst['tau_0']*np.exp(-cst['mu']*np.max(temps-(cst['N']-cst['t_0']),0))
+    S = syst[0] 
+    I_moins = syst[1] 
+    I_plus = syst[2] 
+    R_moins = syst[3] 
+    R_plus_I = syst[4] 
+    H = syst[5] 
+    U = syst[6] 
+    R_plus_H = syst[7] 
+    D = syst[8] 
+    D_R = syst[9]
+    # Dérivées des variables
+    DS = -tau*S*I_moins
+    DI_moins = tau*S*I_moins-cst['lambda_1']*I_moins-(cst['gamma_IR']+cst['gamma_IH']+cst['gamma_IU'])*I_moins
+    DI_plus = cst['lambda_1']*I_moins-(cst['gamma_IR']+cst['gamma_IH']+cst['gamma_IU'])*I_plus
+    DR_moins = cst['gamma_IR']*I_moins-cst['lambda_2']*R_moins
+    DR_plus_I = cst['gamma_IR']*I_plus+cst['lambda_2']*R_moins
+    DH = cst['gamma_IH']*(I_moins + I_plus)-(cst['gamma_HR']+cst['gamma_HD']+cst['gamma_HU'])*H
+    DU = cst['gamma_IU']*(I_moins + I_plus)+cst['gamma_HU']*H-(cst['gamma_UR'] + cst['gamma_UD'])*U
+    DR_plus_H = cst['gamma_HR']*H+cst['gamma_UR']*U
+    DD = cst['gamma_UD']*U+cst['gamma_HD']*H  
+    DD_R = (cst['lambda_1']+cst['gamma_IH']+cst['gamma_IU'])*I_moins-D_R
+    
+    
+    return(DS,DI_moins,DI_plus,DR_moins,DR_plus_I,DH,DU,DR_plus_H,DD,DD_R)
+
+
 
 def pic_epidemie(L):
     #### L une liste 
@@ -24,23 +79,23 @@ def Monte_Carlo(M):
     MAX = []
     N_MAX = []
     for i in range(M):
-        init_constant()
-        Resultat = Resolution_Systeme(T_MAX, deltaT,S_0)
+        print(i)
+        cst = init_constant()
+        syst_CI=array([cst['S_0'],cst['I_moins_0'],0,0,0,0,0,0,0,0]) 
+        Sols=odeint(equa_diff,syst_CI,t)   
         #print(Resultat[6])
-        max_patients, N_jours_max = pic_epidemie(list(Resultat[6]))
+        max_patients, N_jours_max = pic_epidemie(list(Sols[:,6]))
         #print(cst)
         MAX +=[max_patients]
         N_MAX +=[N_jours_max]
     return (MAX,N_MAX)
     
-MAX, N_MAX = Monte_Carlo(10)
-plt.plot(MAX, color = 'r')
-plt.plot(N_MAX)
-plt.show()
-"""
 
-print(init_constant())
-#print(cst['t_0'])
-#print(cst['N'])
-    
+cst = init_constant()
+syst_CI=array([cst['S_0'],cst['I_moins_0'],0,0,0,0,0,0,0,0])
+Sols=odeint(equa_diff,syst_CI,t)
+plt.plot(Sols[:,1])
+ 
+#MAX, N_MAX = Monte_Carlo(100)
+#plt.hist(MAX, bins = 30)    
     
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,26 @@
+from __future__ import division
 import numpy as np
 from datetime import date
 from constant import *
 
 
+from scipy import *
+from pylab import *
 
-def euler_one_step(S,I_moins,I_plus,R_moins,R_plus_I,H,U,R_plus_H,D,D_R):
-    
-    S_1 = S-tau*S*I_moins*deltaT
-    I_moins_1 = I_moins + (tau*S*I_moins-lambda_1*I_moins-(gamma_IR+gamma_IH+gamma_IU)*I_moins)*deltaT
-    I_plus_1 = I_plus +(lambda_1*I_moins-(gamma_IR+gamma_IH+gamma_IU)*I_plus)*deltaT
-    R_moins_1 = R_moins +(gamma_IR*I_moins-lambda_2*R_moins)*deltaT
-    R_plus_I_1 = R_plus_I + (gamma_IR*I_plus+lambda_2*R_moins)*deltaT
-    H_1 = H + (gamma_IH*(I_moins + I_plus)-(gamma_HR+gamma_HD+gamma_HU)*H)*deltaT
-    U_1 = U + (gamma_IU*(I_moins + I_plus)+gamma_HU*H-(gamma_UR + gamma_UD)*U)*deltaT
-    R_plus_H_1 = R_plus_H+ (gamma_HR*H+gamma_UR*U)*deltaT
-    D_1 = D + (gamma_UD*U+gamma_HD*H)*deltaT  
-    D_R_1 = D_R +((lambda_1+gamma_IH+gamma_IU)*I_moins-D_R)*deltaT
+
+
+def euler_one_step(S,I_moins,I_plus,R_moins,R_plus_I,H,U,R_plus_H,D,D_R,i):
+    tau = cst['tau'][i]
+    S_1 = S-cst['tau'][i]*S*I_moins*deltaT
+    I_moins_1 = I_moins + (cst['tau'][i]*S*I_moins-cst['lambda_1']*I_moins-(cst['gamma_IR']+cst['gamma_IH']+cst['gamma_IU'])*I_moins)*deltaT
+    I_plus_1 = I_plus +(cst['lambda_1']*I_moins-(cst['gamma_IR']+cst['gamma_IH']+cst['gamma_IU'])*I_plus)*deltaT
+    R_moins_1 = R_moins +(cst['gamma_IR']*I_moins-cst['lambda_2']*R_moins)*deltaT
+    R_plus_I_1 = R_plus_I + (cst['gamma_IR']*I_plus+cst['lambda_2']*R_moins)*deltaT
+    H_1 = H + (cst['gamma_IH']*(I_moins + I_plus)-(cst['gamma_HR']+cst['gamma_HD']+cst['gamma_HU'])*H)*deltaT
+    U_1 = U + (cst['gamma_IU']*(I_moins + I_plus)+cst['gamma_HU']*H-(cst['gamma_UR'] + cst['gamma_UD'])*U)*deltaT
+    R_plus_H_1 = R_plus_H+ (cst['gamma_HR']*H+cst['gamma_UR']*U)*deltaT
+    D_1 = D + (cst['gamma_UD']*U+cst['gamma_HD']*H)*deltaT  
+    D_R_1 = D_R +((cst['lambda_1']+cst['gamma_IH']+cst['gamma_IU'])*I_moins-D_R)*deltaT
     
     
     return(S_1,I_moins_1,I_plus_1,R_moins_1,R_plus_I_1,H_1,U_1,R_plus_H_1,D_1,D_R_1)
@@ -31,6 +36,8 @@ def Resolution_Systeme(T_MAX, deltaT,S_0):
         Res[:,i] = euler_one_step(Res[0,i-1],Res[1,i-1],Res[2,i-1],Res[3,i-1],Res[4,i-1],Res[5,i-1],Res[6,i-1],Res[7,i-1],Res[8,i-1],Res[9,i-1])
         i+=1
     return(Res)
+
+
 
 
 
