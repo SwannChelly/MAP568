f03f6ed82d6d3b86d083b234d073d8829ae35541
SwannChelly <SwannChelly@users.noreply.github.com>
2021-03-17 23:03:16 +0100

    scipy list

---
+++ src/list.py
@@ -0,0 +1,7 @@
+
+['alpha','anglit','arcsine','argus','beta','betaprime','bradford','burr','burr12','cauchy','chi','chi2','cosine','crystalball','dgamma','dweibull',
+'erlang','expon','exponnorm','exponweib','exponpow','f','fatiguelife','fisk','foldcauchy','foldnorm','genlogistic','gennorm','genpareto',
+'genexpon','genextreme','gausshyper','gamma','gengamma','genhalflogistic','geninvgauss','gilbrat','gompertz','gumbel_r','gumbel_l','halfcauchy','halflogistic','halfnorm','halfgennorm',
+'hypsecant','invgamma','invgauss','invweibull','johnsonsb','johnsonsu','kappa4','ksone','kstwo','kstwobign','laplace','laplace_asymmetric','levy','levy_l','kappa3','levy_stable',
+'logistic','loggamma','loglaplace','lognorm','loguniform','lomax','maxwell','mielke','moyal','nakagami','ncx2','ncf','nct','norm','norminvgauss','pareto','pearson3','powerlaw','powerlognorm','powernorm','rdist','rayleigh','rice',
+'recipinvgauss','semicircular',,'skewnorm','t','trapezoid','triang','truncexpon','truncnorm','tukeylambda','uniform','vonmises','vonmises_line','wald','weibull_min','weibull_max','wrapcauchy']
